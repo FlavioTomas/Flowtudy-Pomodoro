0a393f9cd5b2ed40e4b9799e4a2db58a3b39797b
Flavio <flavio.tomas.villa@gmail.com>
2026-01-07 18:40:12 -0300

    Criando uma função padrao que muda a cor de fundo para reutilizar

--- script.js
+++ script.js
@@ -5,31 +5,37 @@ const longBreakButton = document.querySelector('.js-long-break-button')
 const timerModesButtons = document.querySelectorAll('.button--time-mode')
 const body = document.querySelector('.body')
 const timerDisplay = document.querySelector('.js-timer-display')
+const darkModeToggle = document.querySelector('.js-dark-mode-toggle')
 // Getting CSS styles
 const rootElement = document.documentElement;
 const styles = getComputedStyle(rootElement);
 const colorSurface = styles.getPropertyValue('--color-surface')
 const colorBackground = styles.getPropertyValue('--color-background')
 
-
+// changing the appearance of the timer mode buttons according to the mode
 timerModesButtons.forEach(button => {
     button.addEventListener('click', () => {
-        timerModesButtons.forEach(btn => {btn.classList.remove('active')})
+        timerModesButtons.forEach(btn => { btn.classList.remove('active') })
         button.classList.add('active')
     })
 });
 
+// Changing the background color and the timer display value according to the mode
+function changeBackgroundColor(newColor) {
+    rootElement.style.setProperty('--color-background', newColor);
+}
+
 focusButton.addEventListener('click', () => {
-    body.style.background = "#1E2A3A";
+    changeBackgroundColor("#1E2A3A")
     timerDisplay.innerHTML = '25:00'
 })
-
 breakButton.addEventListener('click', () => {
-    body.style.background = "#1c5341ff";
+    changeBackgroundColor("#1c5341ff")
     timerDisplay.innerHTML = '5:00'
 })
-
 longBreakButton.addEventListener('click', () => {
-    body.style.background = "#461935";
+    changeBackgroundColor("#461935")
     timerDisplay.innerHTML = '15:00'
 })
+
+
