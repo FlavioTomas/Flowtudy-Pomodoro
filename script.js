32591600c1f722803081da1f73be7d82abf7c501
Flavio <flavio.tomas.villa@gmail.com>
2026-01-07 17:02:24 -0300

    Criando variáveis para manipular os estilos do CSS, função changeMode criada, por enquanto ela só muda o fundo dos botões de modos

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// Getting modes buttons
 const focusButton = document.querySelector('.js-focus-button')
 const breakButton = document.querySelector('.js-break-button')
 const longBreakButton = document.querySelector('.js-long-break-button')
+// Getting CSS styles
+const rootElement = document.documentElement;
+const styles = getComputedStyle(rootElement);
+const colorSurface = styles.getPropertyValue('--color-surface')
+
+const changeMode = (button) => {
+    button.style.background = colorSurface;
+}
