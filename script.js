67d001e687b21d401179f2231b6de91cbc1b63d9
Flavio <flavio.tomas.villa@gmail.com>
2026-01-07 18:15:45 -0300

    Substitui a função chageMode por um for each para mudar a aparência dos botões de modos

--- script.js
+++ script.js
@@ -2,11 +2,18 @@
 const focusButton = document.querySelector('.js-focus-button')
 const breakButton = document.querySelector('.js-break-button')
 const longBreakButton = document.querySelector('.js-long-break-button')
+const timerModesButtons = document.querySelectorAll('.button--time-mode')
 // Getting CSS styles
 const rootElement = document.documentElement;
 const styles = getComputedStyle(rootElement);
 const colorSurface = styles.getPropertyValue('--color-surface')
+const colorBackground = styles.getPropertyValue('--color-background')
 
-const changeMode = (button) => {
-    button.style.background = colorSurface;
-}
+
+timerModesButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        timerModesButtons.forEach(btn => {btn.classList.remove('active')})
+        button.classList.add('active')
+    })
+});
+
